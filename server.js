978f95e2cdce28ca793743c425993563c3688787
unknown <maximilianerling@gmail.com>
2021-04-03 13:21:25 +0200

    added register functionality

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
 
 const users = [];
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 app.set("view-engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -20,8 +21,19 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", (req, res) => {
-  req.body.email
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.has(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect("/login")
+  } catch {
+    res.redirect("/register")
+  }
 });
 
 app.listen(3000);
