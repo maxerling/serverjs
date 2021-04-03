947473b6090c824d6482e7269f7a476397dd4910
maxerling <maximilianerling@gmail.com>
2021-04-03 05:20:55 +0200

    login.ejs and register.ejs

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000);
+app.set("view-engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render(index.ejs)
-})
+app.get("/", (req, res) => {
+  res.render(index.ejs);
+});
+
+app.listen(3000);
